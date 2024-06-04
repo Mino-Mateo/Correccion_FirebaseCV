914d262c6ea11f183412aa41525373fc9f68335d
Mateo <minora.mateo@gmail.com>
2024-06-03 21:31:28 -0500

    miau

---
+++ Firebase_CV/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Firebase_CV',
+  webDir: 'www'
+};
+
+export default config;
